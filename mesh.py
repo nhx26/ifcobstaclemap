01568434949af4fcb27e35033fcc282cfb995de3
nhx26 <muhammad.anas.gopee@gmail.com>
2021-06-12 03:15:09 -0700

    finalised code

--- mesh.py
+++ mesh.py
@@ -44,11 +44,11 @@ for ifc_entity in ifc_file.by_type('IfcElement'): #iterating through every ifcel
   shape = geom.create_shape(settings, ifc_entity)
     
   if ifc_entity.is_a('IfcDoor'):
-      meshcolor = [0,255,0,100]
+      meshcolor = [0,255,0,255]
   elif ifc_entity.is_a('IfcStair'):
-      meshcolor = [255,255,0,100]
+      meshcolor = [255,255,0,255]
   else:
-      meshcolor = [0,0,0,90]
+      meshcolor = [0,0,0,255]
 
 
 
@@ -61,17 +61,25 @@ for ifc_entity in ifc_file.by_type('IfcElement'): #iterating through every ifcel
 combined = trimesh.util.concatenate(meshlist)
 
 combined.export('combined.ply')
-
+combined.show()
 
 mesh = pv.read('combined.ply')
 
 for level in levels:
-  single_slice = mesh.slice(normal=[0, 0, 1],origin=[0,0,(level/unitfactor+0.5)])
+
+  slices = mesh.slice_along_axis(n=5, axis="z",bounds=[0,0,0,0,level/unitfactor,level/unitfactor+arsheight])
+  #slices.plot(cmap=cmap, parallel_projection = True, background='white')
+
+  #single_slice = mesh.slice(normal=[0, 0, 1],origin=[0,0,(level/unitfactor+arsheight)])
   p = pv.Plotter()
   p.set_background("white")
-  actor = p.add_mesh(single_slice,show_scalar_bar=False,cmap=['black','green','yellow'])
-  p.set_focus(single_slice.center)
-  p.camera_set = True
+  actor = p.add_mesh(slices,show_scalar_bar=False,cmap=['black','green','blue'])
+  
+  #p.set_focus(slices.center)
+
+  p.camera_position = 'xy'
+  p.camera.SetParallelProjection(True)
+  #p.camera_set = True
   p.show(screenshot='OMs/{0}.png'.format(level))
   p.remove_actor(actor)
 
--- test.py
+++ test.py
@@ -1,7 +1,19 @@
-my_dict={
-	
-	'ok': 'alain'
+import ifcopenshell
 
-}
+ifc_file = ifcopenshell.open('ifcmodels/simplehouse.ifc')
 
-print(my_dict[]['alain'])
+#print(ifc_file())
+
+project = ifc_file.by_type('IfcProject')
+
+walls = ifc_file.by_type('IfcWall')
+
+wall = walls[0]
+
+spaces = ifc_file.by_type('IfcSpace')
+
+space = spaces[0]
+
+for item in ifc_file.get_inverse(space):
+	if item.is_a('IfcRelSpaceBoundary'):
+		#print(item.RelatedBuildingElement)
